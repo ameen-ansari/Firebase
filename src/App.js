c9304621903991b15cb9d6f971b72750ecf8f66e
M.Ameen <ameen55668@gmail.com>
2022-09-13 14:28:31 +0500

    create a user in firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // import { getAuth, connectAuthEmulator, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import  auth  from './Firebase';
 import './App.css';
 import { useState } from 'react';
 
@@ -28,38 +30,12 @@ function App() {
   //     })
   //   })
   // }
-
-
-  // Initialize Firebase Authentication and get a reference to the service
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyA8-Yxyh6Bo0hgcqSgP-YO6LTshETp8Qds",
-  //   authDomain: "fooddx-aa127.firebaseapp.com",
-  //   databaseURL: "https://fooddx-aa127-default-rtdb.firebaseio.com",
-  //   projectId: "fooddx-aa127",
-  //   storageBucket: "fooddx-aa127.appspot.com",
-  //   messagingSenderId: "845272473894",
-  //   appId: "1:845272473894:web:8f29f7a4485c2a9c2361e7"
-  // };
-  const app = initializeApp({
-    apiKey: "AIzaSyA8-Yxyh6Bo0hgcqSgP-YO6LTshETp8Qds",
-    authDomain: "fooddx-aa127.firebaseapp.com",
-    databaseURL: "https://fooddx-aa127-default-rtdb.firebaseio.com",
-    projectId: "fooddx-aa127",
-    storageBucket: "fooddx-aa127.appspot.com",
-    messagingSenderId: "845272473894",
-    appId: "1:845272473894:web:8f29f7a4485c2a9c2361e7"
-  });
-  const auth = getAuth(app);
-
-  const provider = new GoogleAuthProvider();
+  // const provider = new GoogleAuthProvider();
   // connectAuthEmulator(auth,'http://localhost:3000/')
 
   const post = async () => {
-    // const sd = await signInWithEmailAndPassword(auth, inputval, inputval2)
-    // console.log(sd.user);
-    // const sd = await createUserWithEmailAndPassword(auth, inputval, inputval2)
-    // console.log(sd.user);
-    await signInWithPopup(auth, provider)
+    const createu = await createUserWithEmailAndPassword(auth, inputval, inputval2)
+    console.log(createu.user);
      
 
   }
@@ -67,9 +43,10 @@ function App() {
     <>
       <p style={{ textAlign: 'center', color: 'blue', fontWeight: '800', fontSize: '45px' }}> &rarr; &rarr; &rarr; Firebase Learning &larr; &larr; &larr;</p>
       <p style={{ textAlign: 'center', color: 'green', fontWeight: '800', fontSize: '35px' }}> &rarr; By Ameen Ansari &larr;</p>
-      <label htmlFor="name">name:</label>
-      <input type="text" id='name' value={inputval} onChange={input} />
-      <input type="text" id='name' value={inputval2} onChange={input2} />
+      <label htmlFor="name">Enter Email Here</label>
+      <input type="text" id='name' required value={inputval} onChange={input} /><br />
+      <label htmlFor="password" >Enter Password Here</label>
+      <input type="password" id='password' required value={inputval2} onChange={input2} /><br />
       <button onClick={post}>Submit</button>
     </>
   );
---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const app = initializeApp({
+    apiKey: "AIzaSyA8-Yxyh6Bo0hgcqSgP-YO6LTshETp8Qds",
+    authDomain: "fooddx-aa127.firebaseapp.com",
+    databaseURL: "https://fooddx-aa127-default-rtdb.firebaseio.com",
+    projectId: "fooddx-aa127",
+    storageBucket: "fooddx-aa127.appspot.com",
+    messagingSenderId: "845272473894",
+    appId: "1:845272473894:web:8f29f7a4485c2a9c2361e7"
+});
+const auth = getAuth(app);
+export default auth
